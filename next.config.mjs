a067a2bf3f6d84c110f201eaf9418300feb35ee9
Martin Weber <53663335+Marty-W@users.noreply.github.com>
2023-11-26 11:58:48 +0100

    fix: svgr lib typings and get rid of old comp

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
+
+// @ts-expect-error - next-plugin-svgr is not typed
 import withSvgr from "next-plugin-svgr";
 await import("./src/env.mjs");
 
