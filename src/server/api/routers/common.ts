2af79b693a7b3f0df946051a2cca6525ac96c7bb
Martin Weber <53663335+Marty-W@users.noreply.github.com>
2023-12-22 17:35:53 +0100

    fix: remove .prepare, pscale doesnt support it

--- src/server/api/routers/common.ts
+++ src/server/api/routers/common.ts
@@ -1,14 +1,19 @@
 import { createTRPCRouter, publicProcedure } from "../trpc";
-import {
-  getCountryCount,
-  getPresentCountries,
-  getTeamCount,
-} from "@/server/db/statements";
+import { count, countDistinct } from "drizzle-orm";
+import { teams } from "@/server/db/schema";
 export const commonRouter = createTRPCRouter({
-  getTeamsCountInfo: publicProcedure.query(async () => {
-    const teamCount = await getTeamCount.execute();
-    const countryCount = await getCountryCount.execute();
-    const presentCountries = await getPresentCountries.execute();
+  getTeamsCountInfo: publicProcedure.query(async ({ ctx }) => {
+    const teamCount = await ctx.db.select({ value: count() }).from(teams);
+
+    const countryCount = await ctx.db
+      .select({ value: countDistinct(teams.country) })
+      .from(teams);
+
+    const presentCountries = await ctx.db
+      .selectDistinct({
+        country: teams.country,
+      })
+      .from(teams);
     return {
       teamCount: teamCount[0]?.value ?? 0,
       countryCount: countryCount[0]?.value ?? 0,
