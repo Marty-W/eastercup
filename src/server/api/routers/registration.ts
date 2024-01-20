3d6345f6cc0d77c3e4515615b4102845236a1c85
Martin Weber <53663335+Marty-W@users.noreply.github.com>
2024-01-20 21:34:36 +0100

    feat: make registering team tasks parallel

--- src/server/api/routers/registration.ts
+++ src/server/api/routers/registration.ts
@@ -22,23 +22,24 @@ export const registrationRouter = createTRPCRouter({
 
       const newTeamId = await createTeam(info);
 
-      await addTeamTransportInfo(info, newTeamId);
-      await addTeamBillingInfo(billing, newTeamId);
-      await generateRegistrationInvoice(newTeamId, info.country);
-      await generateTshirtOrder(newTeamId, services.tshirtOrder);
+      const parallelTasks = [
+        addTeamTransportInfo(info, newTeamId),
+        addTeamBillingInfo(billing, newTeamId),
+        generateRegistrationInvoice(newTeamId, info.country),
+        generateTshirtOrder(newTeamId, services.tshirtOrder),
+        generateCateringOrder(newTeamId, services.cateringOrder),
+      ];
+
+      await Promise.all(parallelTasks);
 
       const registrationInvoiceVarSymbol = getInvoiceVarSymbol(newTeamId);
 
-      if (services.interestInCatering) {
-        await generateCateringOrder(newTeamId, services.cateringOrder);
-      }
-
-      // NOTE: I don't want to rely on this to success in order to register the team and also this fn should be fast
       if (process.env.VERCEL_ENV === "production") {
         sendPostRegistrationEmail(info.email, info.country);
       }
 
       const diff = new Date().getTime() - timer.getTime();
+
       console.log(`Registration took ${diff}ms`);
 
       return {
