aa1976b7de8584e7fd4f71bd45593cce96b13414
Martin Weber <53663335+Marty-W@users.noreply.github.com>
2024-03-14 16:33:33 +0100

    feat: add api route for deleting invoice blobs

---
+++ src/app/api/deleteInvoiceBlob/route.ts
@@ -0,0 +1,65 @@
+import { del } from "@vercel/blob";
+import { z } from "zod";
+import { env } from "@/env.mjs";
+
+const requestSchema = z.object({
+  blobUrl: z.string(),
+});
+
+export async function DELETE(request: Request) {
+  const authHeader = request.headers.get("Authorization");
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return new Response(JSON.stringify({ error: "Unauthorized" }), {
+      status: 401,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  const apiKey = authHeader.split(" ")[1];
+
+  if (apiKey !== env.BAKERY_API_TOKEN) {
+    return new Response(JSON.stringify({ error: "Invalid API key" }), {
+      status: 401,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  try {
+    const body = await request.json();
+    const validatedBody = requestSchema.parse(body);
+
+    const { blobUrl } = validatedBody;
+
+    await del(blobUrl);
+
+    return new Response(
+      JSON.stringify({
+        message: "Blob deleted successfully.",
+      }),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+  } catch (error) {
+    console.error(error);
+    return new Response(
+      JSON.stringify({
+        error: "Invalid data provided.",
+      }),
+      {
+        status: 400,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+  }
+}
