4c4d6025e93e783948b12fc5301dde8b2d5c8351
Martin Weber <53663335+Marty-W@users.noreply.github.com>
2024-03-21 12:38:03 +0100

    fix: better home info sizing and info layout

--- src/app/[locale]/info/layout.tsx
+++ src/app/[locale]/info/layout.tsx
@@ -5,9 +5,11 @@ interface Props {
 }
 export default function InfoPageLayout({ children }: Props) {
   return (
-    <div className="min-h-[50svh] py-4 font-display md:py-6">
+    <div className="flex min-h-[50svh] flex-col py-4 font-display md:py-6 xl:min-h-[70svh]">
       <InfoPageHeader />
-      <div className="container py-4 md:py-8 lg:py-12">{children}</div>
+      <div className="container flex flex-1 flex-col py-4 md:py-8 lg:py-12">
+        {children}
+      </div>
     </div>
   );
 }
--- src/app/[locale]/info/page.tsx
+++ src/app/[locale]/info/page.tsx
@@ -6,7 +6,7 @@ import { getScopedI18n } from "locales/server";
 export default async function InformationPage() {
   const t = await getScopedI18n("info.home");
   return (
-    <div className="lg:2xl grid grid-cols-2 gap-2 font-display md:gap-4 md:text-xl lg:grid-cols-6 lg:gap-y-6">
+    <div className="lg:2xl grid flex-1 grid-cols-2 gap-2 font-display md:gap-4 md:text-xl lg:grid-cols-6 lg:gap-y-6">
       <BentoCard
         className="group col-span-2 flex items-center justify-center lg:col-span-3"
         href="#"
@@ -32,26 +32,26 @@ export default async function InformationPage() {
         {t("rules")}
       </BentoCard>
       <BentoCard
-        className="flex h-24 items-center justify-center bg-brand-blue text-white lg:col-span-2 lg:col-start-5"
+        className="flex h-24 items-center justify-center bg-brand-blue text-white lg:col-span-2 lg:col-start-5 lg:h-full"
         href="/info/accommodation"
       >
         {t("accommodation")}
       </BentoCard>
       <BentoCard
-        className="flex h-24 items-center justify-center lg:col-span-2 lg:col-start-5 lg:row-start-2"
+        className="flex h-24 items-center justify-center lg:col-span-2 lg:col-start-5 lg:row-start-2 lg:h-full"
         href="/info/catering"
       >
         {t("catering")}
       </BentoCard>
       <BentoCard
-        className="relative flex h-36 items-center justify-center overflow-clip bg-brand-black text-white lg:col-span-4 lg:h-64"
+        className="relative flex h-36 items-center justify-center overflow-clip bg-brand-black text-white lg:col-span-4 lg:h-72"
         href="/info/about"
       >
         {t("about")}
         <AnimatedHeroStripe disableAnimation />
       </BentoCard>
       <BentoCard
-        className="flex items-center justify-center bg-brand-red text-white lg:col-span-2"
+        className="flex items-center justify-center bg-brand-red text-white lg:col-span-2 lg:h-72"
         href="/info/klatovy"
       >
         {t("klatovy")}
