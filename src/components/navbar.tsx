4231ee7c0dc168bc6a017eb4cf51c74bcc8d49e6
Martin Weber <53663335+Marty-W@users.noreply.github.com>
2024-03-21 12:22:17 +0100

    fix: logo fixed again

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,11 +7,17 @@ import NavLinks from "./navLinks";
 
 export default function Navbar() {
   return (
-    <header className="px-4 pt-4 md:px-8 lg:pt-8">
+    <header className="px-4 pt-4 md:px-8 lg:px-14 lg:pt-8">
       <nav className="grid h-[10vh] grid-cols-[1fr_2fr_1fr] lg:min-h-[12vh]">
-        <div className="relative flex h-full w-full items-center justify-self-start">
-          <Link href="/" className="h-full w-full">
-            <Image src="/logo-big.svg" alt="Logo" layout="fill" />
+        <div className="flex h-full w-full items-center justify-self-start">
+          <Link href="/" className="relative h-full w-full">
+            <Image
+              src="/logo-big.svg"
+              alt="Logo"
+              layout="fill"
+              objectFit="contain"
+              objectPosition="left"
+            />
           </Link>
         </div>
         <div className="hidden self-center justify-self-center lg:block">
