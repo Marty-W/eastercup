1af74416235aceca3b9112ea3ba129865cff29c5
Martin Weber <martin8weber@gmail.com>
2024-03-25 18:04:50 +0100

    feat: add vercel speed insights

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import localFont from "next/font/local";
 import { GeistSans } from "geist/font/sans";
